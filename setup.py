61bfa1bd6aabb203ab71e3aad14676ad76103fa0
Jeff McBride <mcbridejc@gmail.com>
2021-04-28 12:28:08 -0700

    Add multi-grid and feedback control support (#1)
    * Support new multi-grid layout and feedback control commands
    
    * Use module variable instead of enums for API constants
    
    Also, pull them up to top-level package scope in __init__.py

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 from setuptools import setup, find_packages
 
-print( find_packages())
 setup(
     name="pdclient",
-    version="0.1.0",
+    version="0.3.0",
     packages=find_packages(),
     entry_points={
         'console_scripts': [
